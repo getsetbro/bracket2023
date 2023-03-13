77db61a6206273a72b508aa4de24e041079d4bb0
Seth Broweleit <seth.broweleit@insight.com>
2023-03-13 19:27:49 -0400

    init

---
+++ js.js
@@ -0,0 +1,34 @@
+let data = [
+  {
+    school: "Alabama",
+    team: "Crimson Tide",
+    logo: "https://a.espncdn.com/combiner/i?img=/i/teamlogos/ncaa/500/333.png",
+    city: "",
+    state: "Alabama",
+    seed: "1",
+  },
+  {
+    school: "Houston",
+    team: "Cougars",
+    logo: "https://a.espncdn.com/combiner/i?img=/i/teamlogos/ncaa/500/248.png",
+    city: "Houston",
+    state: "Texas",
+    seed: "1",
+  },
+  {
+    school: "Kansas",
+    team: "Jayhawks",
+    logo: "https://a.espncdn.com/combiner/i?img=/i/teamlogos/ncaa/500/2305.png",
+    city: "",
+    state: "Kansas",
+    seed: "1",
+  },
+  {
+    school: "Purdue",
+    team: "Boilermakers",
+    logo: "https://a.espncdn.com/combiner/i?img=/i/teamlogos/ncaa/500/2305.png",
+    city: "West Lafayette",
+    state: "Indiana",
+    seed: "1",
+  },
+];
